0010d8dae1e845b25e5dae90c4fbf6af4d003c6d
Rohit <rohit.jbcodeapp@gmail.com>
2023-09-14 15:00:14 +0530

    Building a Blog Post Editor with React and CKEditor

---
+++ Building a Blog Post Editor with React and CKEditor/src/App.js
@@ -0,0 +1,13 @@
+import Editor from "./components/Editor";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Editor />
+      </header>
+    </div>
+  );
+}
+
+export default App;
