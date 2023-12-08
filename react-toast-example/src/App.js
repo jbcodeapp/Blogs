853516272df9bd369b71f068c4a7ae758de38bbf
rohitjbcodeapp <139564077+rohitjbcodeapp@users.noreply.github.com>
2023-12-08 11:30:45 +0530

    add react-toast-example

---
+++ react-toast-example/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Toast from './Toast';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Toast Example</h1>
+      <Toast />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-toast-example/src/Toast.js
@@ -0,0 +1,22 @@
+// Toast.js
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Toast = () => {
+    const showToast = () => {
+        toast('This is a Toast notification!', {
+          autoClose: 2000,
+          style: { background: 'gray', color: 'white' }, // // Customize the background and text color
+        });
+      };
+
+  return (
+    <div>
+      <button onClick={showToast}>Show Toast</button>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Toast;
